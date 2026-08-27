8bf6ada6821d88e1d855480d262797c359f2a755
LucasFiuba1 <lcirillo@fi.uba.ar>
2026-08-27 08:44:53 -0300

    feat(functions): list students by number validation message

--- functions/students-operations.sh
+++ functions/students-operations.sh
@@ -4,14 +4,15 @@ source "./constants.sh"
 source "./env-operations.sh"
 
 list_students_by_number() {
-    if does_outputfile_exist; then
+    if ! does_outputfile_exist; then
+        echo "El archivo $OUTPUT_FILE no existe."
         return 1
+    else
+        echo -e "\nAlumnos ordenados por número de padrón:"
+        echo "--------------------------------------"
+
+        sort -n -k1,1 "$OUTPUT_FILE"
     fi
-
-    echo -e "\nAlumnos ordenados por número de padrón:"
-    echo "--------------------------------------"
-
-    sort -n -k1,1 "$OUTPUT_FILE"
 }
 
 show_top_grades() {
